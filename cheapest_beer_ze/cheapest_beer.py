6df468d6f560e2e09c52f5499c85eb684371b3b8
Bruno Prates <bruno.sp.prates@gmail.com>
2021-05-18 00:22:13 -0300

    included error message when address is invalid

--- cheapest_beer_ze/cheapest_beer.py
+++ cheapest_beer_ze/cheapest_beer.py
@@ -4,9 +4,11 @@ import json
 from cheapest_beer_ze.scraper import BeerScraper
 
 def get_cheapest_beers(address="Rua Visconde de Caravelas, 98"):
-    scraper = BeerScraperV3()
+    scraper = BeerScraper()
     scraper.build_driver()
     scraper.define_address(address)
+    if not scraper.address:
+        return 'Address invalid. Please try again with a valid address.'
     scraper.get_available_brands()
     scraper.scrape_data()
     scraper.create_df()
